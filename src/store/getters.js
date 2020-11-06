f893b361900f52c7e92ca93c38d03850a9d75db4
tam.doan <tam.doan@codecomplete.jp>
2020-11-06 10:58:53 +0700

    init project

---
+++ src/store/getters.js
@@ -0,0 +1,5 @@
+export default {
+    newTask: state => state.newTask,
+    tasks: state => state.tasks.filter((task) => {return !task.completed}),
+    completedTask: state => state.tasks.filter((task) => {return task.completed})
+}
